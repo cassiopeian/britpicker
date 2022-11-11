dd6a9f8be8da415f89f5bff4923c2f26b08342ab
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-11-10 22:10:17 -0500

    create copyCat()
    Finally figured out a way to transfer the content of #ae into #be, as the user types, but each letter is followed by an array comma. Can't get the .join() method to work, I guess because, as MDN says, "If the array has only one item, then that item will be returned without using the separator." And technically, each time the mimic array is displayed, it has only one item. Still trying to figure out a solution.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let mimic = [];
+
 function indentText(event) {
     // if the tab key is pressed
     if (event.key === 'Tab') {
@@ -33,3 +35,30 @@ $('#trash').hover(
         $(this).attr('src', './images/icons/trash.svg');
     }
 );
+
+function copyCat(e) {
+    let letters = $('#ae').html().split('');
+    
+    // prevent duplication of capitalized letters
+    if (e.key === 'Shift') {
+        return;
+    }
+
+    // send the first letter to the mimic array
+    mimic.push(letters.splice(0));
+
+    
+    // mimic = mimic.join(' ');
+
+    console.log(`letters: ${letters}`);
+    console.log(`mimic: ${mimic}`);
+
+
+    // add the letter to the british text
+    $('#be').text(mimic);
+
+    // clear the mimic array
+    mimic = [];
+};
+
+$('#ae').on('keyup', copyCat);
