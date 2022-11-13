9c83f83714c2b639ddc842e3dfe640c372025a7d
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2022-11-13 18:54:07 -0500

    update copyCat() solution
    Okay, yayayay omg it's been, what, a week?? More?
    
    After a bunch of trial and error, I've  f i n a l l y
     figured out a way to transfer the text from #ae to #be as it's typed. I'm going to commit a few of the commented-out failures, so I can look back on them later.
    
    Some notes! I considered string methods like .substring(), when I realized the single-item array was preventing me from using .join() to remove the array commas separating each letter, but strings are immutable, so that was a dud.
    
    So, I thought I'd try to add a random letter into the mimic array, with a for loop, so that could be joined with the spliced letters array letter. Then I tried accessing the desired letter with bracket notation, instead of splicing the "whole" one-item array, and I guess doing so finally enabled me to use the .join() method. Should have figured that out sooner.

--- js/main.js
+++ js/main.js
@@ -36,8 +36,11 @@ $('#trash').hover(
     }
 );
 
+let x = ['x', 'o'];
+
 function copyCat(e) {
     let letters = $('#ae').html().split('');
+    
     
     // prevent duplication of capitalized letters
     if (e.key === 'Shift') {
@@ -45,20 +48,50 @@ function copyCat(e) {
     }
 
     // send the first letter to the mimic array
-    mimic.push(letters.splice(0));
+    // mimic.push(letters.splice(0));
 
-    
+    for (let i = 0; i < letters.length; i++) {
+        // mimic.push(x[0][0] + letters.splice(i)); 
+        
+        // mimic.push(letters[i] + x[0][0]);   
+        mimic.push(letters[i]);      
+        $('#be').text(mimic.join(''));
+        // mimic = [];
+        // console.log(mimic[0][0]);
+    }
+
     // mimic = mimic.join(' ');
 
     console.log(`letters: ${letters}`);
     console.log(`mimic: ${mimic}`);
 
 
+    // console.log(typeof mimic);
+
     // add the letter to the british text
-    $('#be').text(mimic);
+    // $('#be').text(mimic);
 
     // clear the mimic array
     mimic = [];
 };
+
+// function copyCat(e) {
+//     let american = $('#ae').html();
+//     console.log(typeof american);
+
+//     // prevent duplication of capitalized letters
+//     if (e.key === 'Shift') {
+//         return;
+//     }
+
+//     for (let i; i < american.length; i++) {
+
+//     }
+
+//     // get the first letter of the string
+//     let typed = american.substring(0, 1);
+
+//     $('#be').text(typed);
+// };
 
 $('#ae').on('keyup', copyCat);
