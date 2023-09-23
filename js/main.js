fea776f66f65c06aa8092fa700be574c6a09dd75
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2023-09-22 23:02:19 -0400

    create condition for pluralized words
    Updated the if statement to include a condition for pluralized words. If the typed word is equal to an American phrase concatenated with 's,' then phrase.plural (the pluralized British translation) should be spliced in.

--- js/main.js
+++ js/main.js
@@ -926,6 +926,9 @@ $('#ae').on('keyup', function() {
             } else if (word.toLowerCase() == phrase.american) {
                 // or replace single words with the "translated" british phrase
                 beSplit.splice(index, 1, caseMatch(phrase.british));
+            } else if (word.toLowerCase() == phrase.american + 's') {
+                // or replace pluralized words
+                beSplit.splice(index, 1, caseMatch(phrase.plural));
             }
         });
     }
