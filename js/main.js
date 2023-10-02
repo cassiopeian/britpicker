ae51176c79c8f255c07064cc88f6ef05fcdc14f7
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2023-10-01 20:03:42 -0400

    update ability to pluralize phrases
    Added a condition that checks whether a two-word phrase is pluralized, so it can be replaced with the pluralized British phrase.

--- js/main.js
+++ js/main.js
@@ -1020,6 +1020,13 @@ $('#ae').on('keyup', function() {
                 beSplit.splice(index, 1, caseMatch(phrase.british));
                 // remove the redundant (first) word and space before the phrase 
                 aeSplit.splice(prevIndex, 2);
+            } else if (typeof phrase.plural !== 'undefined' && twoWords.toLowerCase() == phrase.american + 's' || word.toLowerCase() == phrase.american + 'es') {
+                // replace the second word with the whole american phrase 
+                aeSplit.splice(index, 1, phrase.american);
+                // replace that phrase with the pluralized british phrase
+                beSplit.splice(index, 1, caseMatch(phrase.plural));
+                // remove the redundant (first) word and space before the phrase 
+                aeSplit.splice(prevIndex, 2);
             } else if (word.toLowerCase() == phrase.american) {
                 // or replace single words with the "translated" british phrase
                 beSplit.splice(index, 1, caseMatch(phrase.british));
