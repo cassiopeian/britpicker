08888450f6d13e14e941f19917013e8910564ac0
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2023-10-05 21:24:06 -0400

    create translations-array.js
    Moved the translations array out of the main.js file, into its own document.

---
+++ js/translations-array.js
@@ -0,0 +1,341 @@
+let translations = [
+    {
+        american: 'ad',
+        british: 'advert',
+        plural: 'adverts'
+    },
+    {
+        american: 'among',
+        british: 'amongst'
+    },
+    {
+        american: 'apartment',
+        british: 'flat',
+        plural: 'flats'
+    },
+    {
+        american: 'an apartment',
+        british: 'a flat'
+    },
+    {
+        american: 'argument',
+        british: 'argy-bargy',
+        plural: 'argy-bargies'
+    },
+    {
+        american: 'arugula',
+        british: 'rocket'
+    },
+    {
+        american: 'an arugula',
+        british: 'a rocket'
+    },
+    {
+        american: 'ass',
+        british: 'arse',
+        plural: 'arses'
+    },
+    {
+        american: 'bartender',
+        british: 'barman',
+        plural: 'barmen'
+    },
+    {
+        american: 'bathing suit',
+        british: 'swimming costume',
+        plural: 'swimming costumes'
+    },
+    {
+        american: 'broke',
+        british: 'skint'
+    },
+    {
+        american: 'burlap',
+        british: 'hessian'
+    },
+    {
+        american: 'chip',
+        british: 'crisp',
+        plural: 'crisps'
+    },
+    {
+        american: 'cilantro',
+        british: 'coriander'
+    },
+    {
+        american: 'cookie',
+        british: 'biscuit',
+        plural: 'biscuits'
+    },
+    {
+        american: 'cooktop',
+        british: 'hob',
+        plural: 'hobs'
+    },
+    {
+        american: 'costume',
+        british: 'fancy dress'
+    },
+    {
+        american: 'counter',
+        british: 'worktop',
+        plural: 'worktops'
+    },
+    {
+        american: 'counterclockwise',
+        british: 'anticlockwise'
+    },
+    {
+        american: 'countertop',
+        british: 'worktop',
+        plural: 'worktops'
+    },
+    {
+        american: 'dollar',
+        british: 'pound',
+        plural: 'pounds'
+    },
+    {
+        american: 'eggplant',
+        british: 'aubergine',
+        plural: 'aubergines'
+    },
+    {
+        american: 'elevator',
+        british: 'lift',
+        plural: 'lifts'
+    },
+    {
+        american: 'field',
+        british: 'pitch',
+        plural: 'pitches'
+    },
+    {
+        american: 'freeway',
+        british: 'motorway',
+        plural: 'motorways'
+    },
+    {
+        american: 'freshman',
+        british: 'fresher',
+        plural: 'freshers'
+    },
+    {
+        american: 'friend',
+        british: 'mate',
+        plural: 'mates'
+    },
+    {
+        american: 'fry',
+        british: 'chip',
+        plural: 'chips'
+    },
+    {
+        american: 'garbage',
+        british: 'rubbish'
+    },
+    {
+        american: 'garbageman',
+        british: 'binman',
+        plural: 'binmen' 
+    },
+    {
+        american: 'gas',
+        british: 'petrol'
+    },
+    {
+        american: 'get along',
+        british: 'get on'
+    },
+    {
+        american: 'hood',
+        british: 'bonnet',
+        plural: 'bonnets'
+    },
+    {
+        american: 'highway',
+        british: 'motorway',
+        plural: 'motorways'
+    },
+    {
+        american: 'journalist',
+        british: 'journo',
+        plural: 'journos'
+    },
+    {
+        american: 'laundromat',
+        british: 'launderette',
+        plural: 'launderettes'
+    },
+    {
+        american: 'mom',
+        british: 'mum',
+        plural: 'mums'
+    },
+    {
+        american: 'national holiday',
+        british: 'bank holiday',
+        plural: 'bank holidays'
+    },
+    {
+        american: 'oatmeal',
+        british: 'porridge',
+        plural: 'porridges'
+    },
+    {
+        american: 'an oatmeal',
+        british: 'a porridge'
+    },
+    {
+        american: 'panty',
+        british: 'knicker',
+        plural: 'knickers'
+    },
+    {
+        american: 'pant',
+        british: 'trouser',
+        plural: 'trousers'
+    },
+    {
+        american: 'paper towels',
+        british: 'kitchen roll'
+    },
+    {
+        american: 'parka',
+        british: 'anorak',
+        plural: 'anoraks'
+    },
+    {
+        american: 'pay raise',
+        british: 'pay rise',
+        plural: 'pay rises'
+    },
+    {
+        american: 'pharmacist',
+        british: 'chemist',
+        plural: 'chemists'
+    },
+    {
+        american: 'pleased',
+        british: 'chuffed'
+    },
+    {
+        american: 'pruning shears',
+        british: 'secateurs'
+    },
+    {
+        american: 'putter',
+        british: 'potter'
+    },
+    {
+        american: 'quiet down',
+        british: 'quieten down'
+    },
+    {
+        american: 'quieted',
+        british: 'quietened'
+    },
+    {
+        american: 'rental',
+        british: 'hire',
+        plural: 'hires'
+    },
+    {
+        american: 'shrimp',
+        british: 'prawn',
+        plural: 'prawns'
+    },
+    {
+        american: 'sidewalk',
+        british: 'pavement',
+        plural: 'pavements'
+    },
+    {
+        american: 'sneaker',
+        british: 'trainer',
+        plural: 'trainers'
+    },
+    {
+        american: 'soccer',
+        british: 'football'
+    },
+    {
+        // soccer ball
+        american: 'football ball',
+        british: 'football',
+        plural: 'footballs'
+    },
+    {
+        // soccer player
+        american: 'football player',
+        british: 'footballer',
+        plural: 'footballers'
+    },
+    {
+        american: 'stroller',
+        british: 'pram',
+        plural: 'prams'
+    },
+    {
+        american: 'tank top',
+        british: 'singlet',
+        plural: 'singlets'
+    },
+    {
+        american: 'thanks',
+        british: 'cheers'
+    },
+    {
+        american: 'toilet',
+        british: 'loo',
+        plural: 'loos'
+    },
+    {
+        american: 'ton',
+        british: 'shedload',
+        plural: 'shedloads'
+    },
+    {
+        american: 'toward',
+        british: 'towards'
+    },
+    {
+        american: 'track pants',
+        british: 'trackies'
+    },
+    {
+        american: 'trash',
+        british: 'rubbish'
+    },
+    {
+        american: 'truck',
+        british: 'lorry',
+        plural: 'lorries'
+    },
+    {
+        american: 'uniform',
+        british: 'kit',
+        plural: 'kits'
+    },
+    {
+        american: 'vacation',
+        british: 'holiday',
+        plural: 'holidays'
+    },
+    {
+        american: 'while',
+        british: 'whilst'
+    },
+    {
+        american: 'windshield',
+        british: 'windscreen',
+        plural: 'windscreens'
+    },
+    {
+        american: 'wrench',
+        british: 'spanner',
+        plural: 'spanners'
+    },
+];
+
+export { translations };
