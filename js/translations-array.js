d174050070f2f14ef27441211718867bdea2b43d
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2023-10-14 22:14:42 -0400

    add cucumber
    Added cucumber.

--- js/translations-array.js
+++ js/translations-array.js
@@ -110,6 +110,11 @@ let translations = [
         british: 'anticlockwise'
     },
     {
+        american: 'cucumber',
+        british: 'gherkin',
+        plural: 'gherkins'
+    },
+    {
         american: 'cupcake',
         british: 'fairy cake',
         plural: 'fairy cakes'
