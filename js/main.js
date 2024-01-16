4d645ebbd42a5dd224b85175f069adbcf72faa0c
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2024-01-15 23:05:59 -0500

    update twoWords "secret" index
    Multi-word phrases output incorrect index positions, and I guess that's because the index is being logged during the splice. This was breaking the .edit onclick. Subtracting 2 from the index variable seems to work (so far).

--- js/main.js
+++ js/main.js
@@ -190,7 +190,7 @@ $(document).on('keyup change input', '#ae', function() {
                 // replace both words and the space (or hyphen) with the joined american phrase 
                 aeSplit.splice(prevIndex, 3, phrase.american);
                 // replace that with the british phrase and a hidden space
-                beSplit.splice(prevIndex, 1, caseMatch(phrase.british, phrase.american, phrase.alt, index), hiddenSpace);
+                beSplit.splice(prevIndex, 1, caseMatch(phrase.british, phrase.american, phrase.alt, index-2), hiddenSpace);
             } else if (pluralExists && twoWords.toLowerCase() == phrase.american + 's' 
                 || twoWords.toLowerCase() == phrase.american + 'es'
                 || hyphenatedWord.toLowerCase() == phrase.american + 's'
@@ -198,7 +198,7 @@ $(document).on('keyup change input', '#ae', function() {
                 // replace both words and the space with the joined american phrase 
                 aeSplit.splice(prevIndex, 3, phrase.american);
                 // replace that with the pluralized british phrase and a hidden space
-                beSplit.splice(prevIndex, 1, caseMatch(phrase.plural, twoWords, phrase.altplural, index), hiddenSpace);
+                beSplit.splice(prevIndex, 1, caseMatch(phrase.plural, twoWords, phrase.altplural, index-2), hiddenSpace);
             } else if (word.toLowerCase() == phrase.american) {
                 // or replace single words with the "translated" british phrase
                 beSplit.splice(index, 1, caseMatch(phrase.british, word, phrase.alt, index));
