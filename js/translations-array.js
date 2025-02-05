35bbdd35c1ee0e54234661021adf514ee598a482
cassiopeia <65559770+cassiopeian@users.noreply.github.com>
2025-02-05 17:40:39 -0500

    add expensive alt
    Added pricey.

--- js/translations-array.js
+++ js/translations-array.js
@@ -1411,7 +1411,8 @@ let translations = [
     },
     {
         american: 'expensive',
-        british: 'dear'
+        british: 'dear',
+        alt: 'pricey'
     },
     {
         american: 'expiration date',
